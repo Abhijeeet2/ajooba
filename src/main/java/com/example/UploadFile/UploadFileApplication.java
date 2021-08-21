2d97254fc995f1d7bdd6cf828d8a94fd1813dcb2
abhijeet <abhijeetkesarkar1289@gmail.com>
2021-08-21 19:51:32 +0530

    first ever commit

---
+++ src/main/java/com/example/UploadFile/UploadFileApplication.java
@@ -0,0 +1,15 @@
+package com.example.UploadFile;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+@EnableAutoConfiguration
+public class UploadFileApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UploadFileApplication.class, args);
+	}
+
+}
